23626c9dc6295042d4785a7eba71ba8f6490b73b
ezeiberg33 <89863575+ezeiberg33@users.noreply.github.com>
2024-12-17 19:17:43 -0600

    Rename folder

--- detection/ros2_sample/build/sample_pubsub/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/epue/capstone.git/capstone/ros2_sample/install/sample_pubsub'
--- detection/ws_ros2_opencv/build/ros2_opencv/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/epue/capstone.git/capstone/ws_ros2_opencv/install/ros2_opencv'
